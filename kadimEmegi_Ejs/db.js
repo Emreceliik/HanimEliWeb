f84173ade3a06884d859b3808ab5c7b61d1d672a
Hüseyin Aslan <kevseraslan098@gmail.com>
2024-12-30 18:51:22 +0300

    hata düzeltildi

--- kadimEmegi_Ejs/db.js
+++ kadimEmegi_Ejs/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'pass1234',
+    password: 'pass123',
     database: 'kadin_emegi'
 });
 
